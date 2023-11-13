b2985078b393b56d40f359ef0052a15a6eb1d7a7
johrielrsrd <johrielrsrd@gmail.com>
2023-11-13 19:29:59 +0800

    added input function.

--- FollowingPattern.java
+++ FollowingPattern.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class FollowingPattern {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int input = scan.nextInt();
+
         int k = 0;
 
-        for (int i = 1; i <= 5; i++, k = 0) {
-            for (int j = 1; j <= 5 - i; j++) {
+        for (int i = 1; i <= input; i++, k = 0) {
+            for (int j = 1; j <= input - i; j++) {
                 System.out.print("  ");
             }
             while (k != 2 * i - 1) {
